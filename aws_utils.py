ecde49be2974e2e7d59cea8677e3edd3d7f83a48
maveriq <harisjabbar@gmail.com>
2025-02-06 19:33:11 +0100

    ver 2.0

---
+++ aws_utils.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import boto3
+
+session = boto3.session.Session(
+    aws_access_key_id=st.secrets['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key=st.secrets['AWS_SECRET_ACCESS_KEY'],
+    aws_session_token=st.secrets['AWS_SESSION_TOKEN'],
+)
+
+bedrockClient = session.client('bedrock-agent-runtime', 'us-west-2')
+
+
+@st.cache_data(show_spinner="Searching...")
+def getAnswers(query):
+    knowledgeBaseResponse = bedrockClient.retrieve_and_generate(
+        input={'text': query},
+        retrieveAndGenerateConfiguration={
+            'knowledgeBaseConfiguration': {
+                'knowledgeBaseId': st.secrets['knowledgeBaseId'],
+                'modelArn': 'arn:aws:bedrock:us-west-2::foundation-model/anthropic.claude-3-5-sonnet-20241022-v2:0',
+                'generationConfiguration': {
+                    'inferenceConfig': {
+                        'textInferenceConfig': {
+                            'maxTokens': 2048,
+                            'temperature': 0.8,
+                        }
+                    }
+                }
+            },
+            'type': 'KNOWLEDGE_BASE'
+        })
+    st.session_state['response'] = knowledgeBaseResponse
+
+
+@st.cache_data(show_spinner="Getting Tags...")
+def getTags(query):
+    pass
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,97 +1,91 @@
 import streamlit as st
-import boto3
-import os
 import pyperclip
-
-# session = boto3.session.Session(
-#     aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
-#     aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
-#     aws_session_token=os.environ['AWS_SESSION_TOKEN'],
-# )
-
-session = boto3.session.Session(
-    aws_access_key_id=st.secrets['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key=st.secrets['AWS_SECRET_ACCESS_KEY'],
-    aws_session_token=st.secrets['AWS_SESSION_TOKEN'],
-)
-
-bedrockClient = session.client('bedrock-agent-runtime', 'us-west-2')
-
-
-@st.cache_data(show_spinner="Searching...")
-def getAnswers(questions):
-    knowledgeBaseResponse = bedrockClient.retrieve_and_generate(
-        input={'text': questions},
-        retrieveAndGenerateConfiguration={
-            'knowledgeBaseConfiguration': {
-                'knowledgeBaseId': st.secrets['knowledgeBaseId'],
-                'modelArn': 'arn:aws:bedrock:us-west-2::foundation-model/anthropic.claude-3-5-sonnet-20241022-v2:0',
-                'generationConfiguration': {
-                    'inferenceConfig': {
-                        'textInferenceConfig': {
-                            'maxTokens': 2048,
-                            'temperature': 0.5,
-                        }
-                    }
-                }
-            },
-            'type': 'KNOWLEDGE_BASE'
-        })
-    return knowledgeBaseResponse
-
+import botocore
+from aws_utils import getAnswers, getTags
+import uuid
 
 # Page Config
-st.set_page_config(page_title="Hallo Hub")  # , layout="wide")
+st.set_page_config(page_title="Hallo Hub", layout="wide")
 
-st.title('🎈 Hallo! Hub')
+if 'sessions' not in st.session_state:
+    st.session_state['sessions'] = []
+
+if 'response' not in st.session_state:
+    st.session_state['response'] = {}
+
+# Sidebar
+st.sidebar.image('assets/dsee_logo.png', width=100)
+st.sidebar.title('🎈 Hallo! Hub')
+
+# st.sidebar.text_input('Feedback')
 
 # Search Bar
-query = st.text_input("Geben Sie hier Ihre Anfrage ein",
-                      "")
+st.subheader("Geben Sie hier Ihre Anfrage ein")
+query = st.text_area("Geben Sie hier Ihre Anfrage ein",
+                     "", label_visibility="hidden", key='text')
 
 if query == "":
     st.stop()
 
-response = getAnswers(query)
+if query not in st.session_state['sessions']:
+    st.session_state['sessions'].append(query)
+try:
+    getAnswers(query)
+except botocore.exceptions.EndpointConnectionError:
+    st.error('Error getting answer')
 
-# Suggested Answer
-st.subheader("Vorgeschlagene Antwort")
+st.sidebar.markdown('Sitzungsverlauf')
+with st.sidebar.container(height=600):
+    for session in st.session_state['sessions']:
+        # st.write()
+        st.button(session[:40], on_click=getAnswers(
+            session), use_container_width=True, key=str(uuid.uuid4()))
+
+
+def clear_text():
+    st.session_state["text"] = ""
+    st.session_state['sessions'] = []
+
+
+st.sidebar.button('Verlauf löschen', disabled=False, on_click=clear_text)
+
+st.sidebar.markdown('Help: mailto://help@d-s-e-e.de')
+st.sidebar.markdown('App Ver : 0.1.0')
+
+st.subheader("Vorgeschlagene Antwort")  # Suggested Answer
 with st.container(border=True):
-    st.markdown(response['output']['text'])
+    st.markdown(st.session_state['response']['output']['text'])
+    feedback = st.feedback()
 
 # Action Buttons
-col1, col2, col3, _ = st.columns([1, 1, 1, 2])
+col1, col2, col3, _ = st.columns([1, 1, 1, 1])
 with col1:
     if st.button("📋 Kopieren", key='copy_button', disabled=True):
-        pyperclip.copy(response['output']['text'])
+        pyperclip.copy(st.session_state['response']['output']['text'])
         st.toast("Copied to clipboard!")
 with col2:
     st.button("✏️ Bearbeiten", key='edit_button', disabled=True)
 with col3:
-    if st.button("🔄 Verfeinern", key='refine_button', disabled=True):
-        response = getAnswers(query)
+    if st.button("🔄 Verfeinern", key='refine_button', disabled=False):
+        getAnswers(" "+query.strip())
 
 # Related Topics
 # st.subheader("Related topics")
 # st.write(" ")
-# col1, col2, col3, col4 = st.columns(4)
-# with col1:
-#     st.write("Theme tag")
-# with col2:
-#     st.write("Theme tag")
-# with col3:
-#     st.write("Theme tag")
-# with col4:
-#     st.write("Theme tag")
+# tags = getTags(query)
+# cols = st.columns(4)
+# for col, tag_val in zip(cols, *list(tags.items())):
+#     with col:
+#         st.markdown(tag_val)
+#         st.button(tag_val[0],type='tertiary') # add color based on confidence level
 
 # Info Sources
 st.subheader("Für diese Antwort verwendete Quellen")
-info_sources = response['citations'][0]['retrievedReferences']
+info_sources = st.session_state['response']['citations'][0]['retrievedReferences']
 for id, source in enumerate(info_sources):
     quote = source['content']['text']
     location = source['location']['s3Location']['uri'].lstrip(
         's3://dsee-data/hackathon/')
     with st.expander(f"Quelle {id+1} : {location}"):
         st.write(f'Relevanter Text in der Quelle :\t"{quote}"')
-        # st.write(f"Source : {location}")
         st.link_button("Zur Quelle >", "#", disabled=True)
